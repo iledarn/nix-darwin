62e46f4ac36024b8ee2da5625e65232aeb24be12
Ildar Nasyrov <iledarnp@gmail.com>
2025-07-13 15:01:42 +0300

    Initial Commit

---
+++ darwin-configuration.nix
@@ -0,0 +1,22 @@
+{pkgs, username, ...}:
+{
+nix.settings = {
+experimental-features = [ "nix-command" "flakes" ];
+};
+ids.gids.nixbld = 350;
+system.stateVersion = 4;
+nixpkgs.config.allowUnfree = true;
+
+environment.systemPackages = with pkgs; [
+wget
+];
+
+users.users.${username} = {
+home="/Users/${username}";
+};
+
+home-manager.users.${username} = {pkgs, ...}: {
+home.stateVersion = "25.05";
+home.packages = with pkgs; [zoxide tmux starship];
+};
+}
---
+++ home.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  # Specify the version of Home Manager you are targeting.
+  # Update this if you switch branches or after release upgrades.
+  home.stateVersion = "25.05";
+
+  # Packages that should be installed for your user. Feel free to extend.
+  home.packages = with pkgs; [
+    zoxide
+    tmux
+    starship
+telegram-desktop
+git
+  ];
+programs.git = {
+    enable = true;
+    userName = "Ildar Nasyrov";
+    userEmail = "iledarnp@gmail.com";
+    aliases = {
+      co = "checkout";
+      st = "status";
+      hist = "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
+    };
+  };
+}
